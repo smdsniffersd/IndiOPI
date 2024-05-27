7d8e71c5a03e77c15914116dcaca2636fed08698
smdsniffersd <andrey23432224@gmail.com>
2024-05-27 16:08:49 +0300

    Add output file

---
+++ outpu.cpp
@@ -0,0 +1,6 @@
+#include "output.h"
+#include <iostream>
+
+void output::output_data(int NumberTea) {
+    std::cout << "Максимальное количество чайников, которые можно использовать: " << NumberTea;
+}
---
+++ output.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace output {
+    /// <summary>
+    /// ¬ывод конечных данных пользователю
+    /// </summary>
+    /// <param name="NumberTea">ћаксимальное количество чайников.</param>
+    void output_data(int NumberTea);
+}
